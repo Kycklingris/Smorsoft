5e4b6989f8ab35463fa4b12598a4decba41c6199
BuildTools <unconfigured@null.spigotmc.org>
2020-11-20 17:10:44 +0100

    Changed mobile menu

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function hamMenu() {
-  var x = document.getElementById("navbar");
-  if (x.style.display === "block") {
+  var x = document.getElementById("hamscreen");
+  if (x.style.display === "flex") {
     x.style.display = "none";
   } else {
-    x.style.display = "block";
-  }
+    x.style.display = "flex";
+    }
 }
