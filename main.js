5a26317113d5cf7d78d27ba325020b032269c986
BuildTools <unconfigured@null.spigotmc.org>
2020-11-22 19:07:51 +0100

    Changed javascript to temporary until fixed

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ function itemExpand(id) {
   var y = document.getElementsByClassName("item")[id - 1];
   var b = document.getElementsByClassName("item-buttons")[id - 1];
   var d = document.getElementsByClassName("content")[0]
+  y.style.display = "none"
+  /*
   if (y.style.position === "fixed") {
     y.style.position = "relative"
     y.style.width = "100%"
@@ -30,4 +32,5 @@ function itemExpand(id) {
     y.style.zIndex = "1"
     d.style.zindex = "4"
   }
+  */
 }
