f1b24761f8029a7e96ee2db448b118364a33bb7c
BuildTools <unconfigured@null.spigotmc.org>
2020-11-22 18:53:47 +0100

    Changed store items

--- main.js
+++ main.js
@@ -9,17 +9,25 @@ function hamMenu() {
 
 function itemExpand(id) {
   var y = document.getElementsByClassName("item")[id - 1];
-  var b = y.getElementsByClassName("item-buttoms")[0];
+  var b = document.getElementsByClassName("item-buttons")[id - 1];
+  var d = document.getElementsByClassName("content")[0]
   if (y.style.position === "fixed") {
     y.style.position = "relative"
     y.style.width = "100%"
     y.style.backgroundColor = "rgba(255, 255, 255, 0.05)";
     y.style.margin = "auto";
+    y.style.zIndex = "5"
+    b.style.display = "none"
+    d.style.backgroundColor = "transparent"
+    d.style.zindex = "0"
   } else {
     y.style.position = "fixed"
     y.style.width = "50%"
     y.style.backgroundColor = "black";
     y.style.margin = "auto"
-    b.style.display = ""
+    b.style.display = "block"
+    d.style.backgroundColor = "black"
+    y.style.zIndex = "1"
+    d.style.zindex = "4"
   }
 }
