e6eed8e4585cc597ffa0e84a491fa0cd9a17b8fe
BuildTools <unconfigured@null.spigotmc.org>
2020-11-22 18:24:24 +0100

    Changed images and partially working item press

--- main.js
+++ main.js
@@ -6,3 +6,20 @@ function hamMenu() {
     x.style.display = "flex";
     }
 }
+
+function itemExpand(id) {
+  var y = document.getElementsByClassName("item")[id - 1];
+  var b = y.getElementsByClassName("item-buttoms")[0];
+  if (y.style.position === "fixed") {
+    y.style.position = "relative"
+    y.style.width = "100%"
+    y.style.backgroundColor = "rgba(255, 255, 255, 0.05)";
+    y.style.margin = "auto";
+  } else {
+    y.style.position = "fixed"
+    y.style.width = "50%"
+    y.style.backgroundColor = "black";
+    y.style.margin = "auto"
+    b.style.display = ""
+  }
+}
